b2e366aac2a0158acc93f4ebd2ba1ad4b674abb8
Student <example@example.com>
2020-01-15 10:19:50 -0800

    Add initial files

---
+++ .spec
@@ -0,0 +1,14 @@
+Employee Tracker
+Build an app for a company to help track employees and the current projects they're assigned. Each employee can have many projects and each project can have many employees. Before coding, think through the application's design using SQL Designer to visualize the relationships between tables and models.
+
+As an HR manager, I want to be able to create divisions in the company (HR, IT, Sales, etc).
+As an HR manager, I want to be able to list, update, and delete divisions.
+As an HR manager, I want to be able to add employees and assign them to a division. An employee will belong to one division and a division will have many employees.
+As an HR manager, I want to be able to choose an individual division and see its employees.
+As a project manager, I want to be able to add projects.
+As a project manager, I want to be able to list the current projects.
+As a project manager, I want to be able to assign projects to an employee when I am looking at the employee page.
+As a project manager, I want to be able to remove employees from projects when their part is completed or they have moved on.
+As a project manager, I want to be able to update which projects an employee is working on.
+As a project manager, I want to see all of the employees on a given project.
+Think about the HR Manager, Project Manager and employees. Are there other user stories to develop?
---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_01_15_163746) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "divisions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employee_projects", id: false, force: :cascade do |t|
+    t.bigint "employee_id"
+    t.bigint "project_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_employee_projects_on_employee_id"
+    t.index ["project_id"], name: "index_employee_projects_on_project_id"
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.integer "division_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "employees", "divisions"
+end
