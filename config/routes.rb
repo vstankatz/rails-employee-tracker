84417b4c0051abf5eed53b9e2279ca3e04d667de
Student <example@example.com>
2020-01-15 12:54:26 -0800

    Add resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'divisions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :divisions do
+    resources :employees
+  end
+  resource :employees
+  resource :projects
 end
